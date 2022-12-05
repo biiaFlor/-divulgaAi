894d1f9f9e6f911a560e635b1f6a8f52f4ef697f
Flor <biiaflor_ifsp@hotmail.com>
2022-12-05 19:41:25 -0300

    Adjusts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ExploreComponent } from './explore/explore.component';
-import { TopbarComponent } from './topbar/topbar.component';
 import { CompanyComponent } from './company/company.component';
-import { ProductComponent } from './product/product.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { ShowCompanyComponent } from './company/show-company/show-company.component';
-import { ShowProductComponent } from './product/show-product/show-product.component';
+import { ExploreComponent } from './explore/explore.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { RegisterComponent } from './register/register.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FormProductComponent } from './product/form-product/form-product.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +26,7 @@ import { ShowProductComponent } from './product/show-product/show-product.compon
     RegisterComponent,
     ExploreComponent,
     ShowCompanyComponent,
-    ShowProductComponent
+    FormProductComponent
   ],
   imports: [
     BrowserModule,
